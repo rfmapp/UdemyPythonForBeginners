d2be19b1234f2126b358317dc3cfe3ad7c61222d
Rafael Marques <rfmapp@gmail.com>
2015-10-20 14:51:12 -0300

    Adicionando código sobre declarações Elif

---
+++ app7ElifStatements.py
@@ -0,0 +1,13 @@
+__author__ = 'rafael'
+
+# Declarações condicionais com Elif.
+# Variáveis para os exemplos.
+a = 2;
+b = 4;
+# Declaração de condições de comparação.
+if a < b:
+    print("A é menor que B.")
+elif a > b:
+    print("A é maior que B.")
+else:
+    print("A é igual a B.")
