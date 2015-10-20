171c673b71c1a6c8c6ea298d78e985188fa55b38
Rafael Marques <rfmapp@gmail.com>
2015-10-20 14:46:17 -0300

    Adicionando código sobre declarações Else

---
+++ app6ElseStatements.py
@@ -0,0 +1,15 @@
+__author__ = 'rafael'
+
+# Declarações condicionais com Else.
+# Variáveis para os exemplos.
+a = 2;
+b = 4;
+# Declaração de condições de comparação.
+if a == b:
+    print("A é igual a B.")
+else:
+    print("A é diferente de B")
+if a < b:
+    print("A é menor que B.")
+else:
+    print("A é maior que B.")
