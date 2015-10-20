c295c7f8a58b3e78bac396ace553f9cc5b60c787
Rafael Marques <rfmapp@gmail.com>
2015-10-20 11:27:20 -0300

    Adicionando código sobre declarações If

---
+++ app5IfStatements.py
@@ -0,0 +1,19 @@
+__author__ = 'rafael'
+
+# Declarações condicionais com If.
+# Variáveis para os exemplos;
+a = 2;
+b = 4;
+# Declaração das condições de comparação.
+if a == b:
+    print("A é igual a B.")
+if a != b:
+    print("A é diferente de B.")
+if a < b:
+    print("A é menor que B.")
+if a > b:
+    print("A é maior que B.")
+if a <= b:
+    print("A é menor ou igual a B.")
+if a >= b:
+    print("A é maior ou igual a B.")
