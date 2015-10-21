457521016771b5dd90a0978eba2572d80d508566
Rafael Marques <rfmapp@gmail.com>
2015-10-21 11:05:45 -0300

    Adicionando código sobre estrutura de controle com for.

---
+++ app10ForLoops.py
@@ -0,0 +1,10 @@
+__author__ = 'rafael'
+
+# Estruturas de controle com For.
+# Variáveis para exemplos.
+names = ["Rafael", "Daniel", "André"]
+# Declaração da estrutura de controle.
+for data in names:
+    print(data)
+
+print("My name and my brother's names.")
