57e8b78b448427b8f51d8673846ea2d7bb77cdda
Rafael Marques <rfmapp@gmail.com>
2015-10-21 11:27:51 -0300

    Adicionando código sobre interrupção e continuidade de execução do código com Break, Continue e Else.

---
+++ app12BreakContinueElse.py
@@ -0,0 +1,26 @@
+__author__ = 'rafael'
+
+# Interrupção e continuidade de código.
+list = [1,2,3,4,5,6,7,8,9]
+for int in list:
+    if int == 7: # Testa a condição.
+        break # O código é interrompido caso a condição seja atendida.
+    else:
+        print(int) # Se a condição não é atendida, o código é executado.
+
+
+# list = [1,2,3,4,5,6,7,8,9]
+# for int in list:
+#     if int == 7: # Testa a condição.
+#         continue # O código "pula" para o próximo teste caso a condição seja atendida.
+#     else:
+#         print(int) # Se a condição não é atendida, o código é executado.
+
+# list = [1,2,3,4,5,6,7,8,9]
+# for int in list:
+#     if int == 7: # Testa a condição.
+#         continue # O código "pula" para o próximo teste caso a condição seja atendida.
+#     else:
+#         print(int) # Se a condição não é atendida, o código é executado.
+# else:
+#     print("Default") # Isso é executado caso haja algum problema com o loop.
