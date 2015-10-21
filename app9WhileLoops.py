6b078ce315e326012d33e61f1ae01e1044b24186
Rafael Marques <rfmapp@gmail.com>
2015-10-21 11:00:15 -0300

    Adicionando código sobre estrutura de controle com while

---
+++ app9WhileLoops.py
@@ -0,0 +1,11 @@
+__author__ = 'rafael'
+
+# Estruturas de controle com While.
+# Variáveis para exemplos.
+a = 0
+# Declaração da estrutura de controle.
+while a < 10:
+    print(a)
+    a += 1
+
+print("This is finished!")
