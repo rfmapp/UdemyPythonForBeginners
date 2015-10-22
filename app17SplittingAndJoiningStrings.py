cf8be3a7a49688b80a09b54447073c5fe8907429
Rafael Marques <rfmapp@gmail.com>
2015-10-22 16:51:18 -0300

    Apresenta mais duas funções para trabalhar com strings, uma que une e outra que separa strings.

---
+++ app17SplittingAndJoiningStrings.py
@@ -0,0 +1,15 @@
+__author__ = 'rafael'
+
+# Demonstrando funções para unir e separar strings.
+string = '-'
+sequence = ['a','b','c','d','e','f']
+# Unindo
+print(string.join(sequence))
+
+# Linha em branco para melhorar a leitura.
+print()
+
+# Nova string
+string2 = 'a-b-c-d-e-f'
+# Separando. O resultado final em forma de uma lista.
+print(string2.split('-'))
