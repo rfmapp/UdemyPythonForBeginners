0948b808221b89f7618c4bd0e60f5f8ec04a718c
Rafael Marques <rfmapp@gmail.com>
2015-10-22 16:28:03 -0300

    Captura da entrada de dados do usuário e exibição do que ele digitou.

---
+++ app15DataInput.py
@@ -0,0 +1,7 @@
+__author__ = 'rafael'
+
+# Captura de dados do usuário.
+# Variável que receberá os dados recebe a entrada de dados.
+data = input("Qual o seu nome?")
+# Exibe mensagem com o dado capturado.
+print("Olá {}.".format(data))
