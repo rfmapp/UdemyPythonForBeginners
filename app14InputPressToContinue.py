1c7ab93ba2f6d6eac4ac54ab08e1f2c0d2237ab0
Rafael Marques <rfmapp@gmail.com>
2015-10-22 16:22:13 -0300

    Introdução à entrada de dados. O código apenas solicita que o usuário pressione qualquer tecla e exibe uma mensagem.

---
+++ app14InputPressToContinue.py
@@ -0,0 +1,6 @@
+__author__ = 'rafael'
+
+# Introdução à entrada de dados pelo usuário.
+# Mensagem solicitando a entrada do usuário.
+input("Pressione qualquer tecla para continuar.")
+print("Obrigado!")
